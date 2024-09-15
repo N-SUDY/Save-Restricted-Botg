3921946610f4b78436b7da27de22f67d942e1d03
Flixjod <144277345+Flixjod@users.noreply.github.com>
2024-09-15 12:52:29 +0530

    Update db.py

--- database/db.py
+++ database/db.py
@@ -3,3 +3,5 @@ from config import DB_URI
 
 mongo_client = MongoClient(DB_URI)
 database = mongo_client.restricted_save
+users_collection = database['users']
+sessions_collection = database['sessions']
