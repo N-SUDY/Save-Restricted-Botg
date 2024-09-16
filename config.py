d066b730a5a15bfcf2d62c4bdbaf4adb53d882cf
Flixjod <144277345+Flixjod@users.noreply.github.com>
2024-09-16 12:10:29 +0530

    Update config.py

--- config.py
+++ config.py
@@ -9,6 +9,9 @@ API_ID = int(os.environ.get("API_ID", ""))
 #Your API Hash from my.telegram.org
 API_HASH = os.environ.get("API_HASH", "")
 
+#Admin IDs
+ADMIN_ID = list(map(int, os.getenv("ADMIN_ID", "").split(",")))
+
 #Database 
 DB_URI = os.environ.get("DB_URI", "")
 
