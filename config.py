08a09da87c2a5e8ee06e304de93d53c435454ab7
Flixjod <144277345+Flixjod@users.noreply.github.com>
2024-09-16 11:47:19 +0530

    Update config.py

--- config.py
+++ config.py
@@ -6,9 +6,6 @@ BOT_TOKEN = os.environ.get("BOT_TOKEN", "")
 #Your API ID from my.telegram.org
 API_ID = int(os.environ.get("API_ID", ""))
 
-#Admin IDs
-ADMIN_ID = list(map(int, os.getenv("ADMIN_ID", "").split(",")))
-
 #Your API Hash from my.telegram.org
 API_HASH = os.environ.get("API_HASH", "")
 
